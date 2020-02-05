a178ce5594a2205d70a8abae76491b65f64cb644
JoeyVinyard <vinyardjoseph@gmail.com>
2020-02-05 18:17:33 -0500

    Componentized register and login

--- public/src/App.js
+++ public/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -21,6 +27,8 @@ import BoardSettings from './BoardSettings.js';
 
 // Firebase imports
 import firebase, { auth, provider } from './Firebase.js'
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function loginWithGoogle() {
     auth.signInWithPopup(provider).then(function(result) {
@@ -64,80 +72,35 @@ function App() {
     const classes = useStyles();
 
     return (
-        <div>
+        <Router>
             <NavBar onLandingPage='true' />
-            <div className={classes.loginBody}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>
-                            <h2>Welcome to Managemit</h2>
-                            <p>Keep track of your tasks and work collaboratively. Log in or register to get started.</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.loginPaper}>
+            <Switch>
+                <Route path="/login">
+                    <Login></Login>
+                </Route>
+                <Route path="/register">
+                    <Register></Register>
+                </Route>
+            </Switch>
+        </Router>
 
-                            <h2>Log in</h2>
 
-                            <Grid container spacing={3}>
-                                <Grid item xs={12}>
-                                    <TextField label='Email' type='email' variant='outlined' className={classes.textField} />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField label='Password' variant='outlined' type='password' className={classes.textField} />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button variant='contained' color='primary' className={classes.button}>Log in</Button>
-                                    <Button variant='contained' className={classes.button} onClick={loginWithGoogle} >Log in with Google</Button>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <ResetPasswordDialog />
-                                </Grid>
-                            </Grid>
-
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.loginPaper}>
-
-                            <h2>Register</h2>
-
-                            <Grid container spacing={3}>
-                                <Grid item xs={12}>
-                                    <TextField label='Email' type='email' variant='outlined' className={classes.textField} />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField label='Password' type='password' variant='outlined' className={classes.textField} />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField label='Re-enter password' type='password' variant='outlined' className={classes.textField} />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button variant='contained' color='primary' className={classes.button}>Register</Button>
-                                </Grid>
-                            </Grid>
-
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-
-            <div>
-                <br /><br /><br /><br />everything below is a testing area<br /><br /><br /><br />
-                <p>Boards page WIP:</p>
-                <NavBar />
-                <BoardsActions />
-                <BoardCardCollection />
-                <p>Board page WIP:</p>
-                <NavBar onBoardPage='true' />
-                <BoardActions/>
-                <ColumnGroup />
-                <p>Board settings page WIP:</p>
-                <NavBar />
-                <BoardSettingsBreadcrumbs boardName='Placeholder name' />
-                <BoardSettings />
-            </div>
-        </div>
+        //     <div>
+        //         <br /><br /><br /><br />everything below is a testing area<br /><br /><br /><br />
+        //         <p>Boards page WIP:</p>
+        //         <NavBar />
+        //         <BoardsActions />
+        //         <BoardCardCollection />
+        //         <p>Board page WIP:</p>
+        //         <NavBar onBoardPage='true' />
+        //         <BoardActions/>
+        //         <ColumnGroup />
+        //         <p>Board settings page WIP:</p>
+        //         <NavBar />
+        //         <BoardSettingsBreadcrumbs boardName='Placeholder name' />
+        //         <BoardSettings />
+        //     </div>
+        // </div>
     );
 }
 
---
+++ public/src/pages/Register.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import NavBar from '../NavBar';
+import ResetPasswordDialog from '../ResetPasswordDialog';
+
+const useStyles = makeStyles(theme => ({
+    loginBody: {
+        margin: 20,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary
+    },
+    loginPaper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 100 + '%' // Keeps the boxes at the same height
+    },
+    button: {
+        marginRight: 5,
+        marginLeft: 5
+    },
+    textField: {
+        width: 80 + '%'
+    }
+}));
+
+function Register(props) {
+    const classes = useStyles();
+    return (
+        <div className={classes.loginBody}>
+            <Grid container justify="center" xs={12}>
+                <Grid container spacing={3} xs={4}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <h2>Welcome to Managemit</h2>
+                            <p>Keep track of your tasks and work collaboratively. Log in or register to get started.</p>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12}>
+                         <Paper className={classes.loginPaper}>
+                             <h2>Register</h2>
+                             <Grid container spacing={3}>
+                                 <Grid item xs={12}>
+                                     <TextField label='Email' type='email' variant='outlined' className={classes.textField} />
+                                 </Grid>
+                                 <Grid item xs={12}>
+                                     <TextField label='Password' type='password' variant='outlined' className={classes.textField} />
+                                 </Grid>
+                                 <Grid item xs={12}>
+                                     <TextField label='Re-enter password' type='password' variant='outlined' className={classes.textField} />
+                                 </Grid>
+                                 <Grid item xs={12}>
+                                     <Button variant='contained' color='primary' className={classes.button}>Register</Button>
+                                 </Grid>
+                             </Grid>
+                         </Paper>
+                     </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default Register;
