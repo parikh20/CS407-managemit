24dd2b6590b25a24d8b6a901f33f46c0d6506ccf
Jaineek Parikh <jaineekparikh@yahoo.com>
2020-02-08 18:00:20 -0500

    Added Login feedback

--- public/src/pages/pageBody/Login.js
+++ public/src/pages/pageBody/Login.js
@@ -5,6 +5,8 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar'
 import ResetPasswordDialog from '../component/ResetPasswordDialog';
 import { Link } from 'react-router-dom';
 
@@ -12,17 +14,38 @@ import getStyles from '../../styling/getStyles';
 
 import { auth, provider } from '../../Firebase.js'
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant='filled' {...props} />;
+}
+
 function Login(props) {
     const classes = getStyles();
     const history = useHistory();
 
+    const regexp = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const [loginEmailError, setLoginEmailError] = React.useState(false);
+    const [loginEmailErrorHelperText, setLoginEmailErrorHelperText] = React.useState('');
+    const [loginPasswordError, setLoginPasswordError] = React.useState(false);
+    const [loginPasswordErrorHelperText, setLoginPasswordErrorHelperText] = React.useState('');
+    const [loginErrorSnackbar, setloginErrorSnackbar] = React.useState(false);
+
     const signInWithEmailAndPassword = (email, password) => {
-        auth.signInWithEmailAndPassword(email, password).then(result => {
-            localStorage.setItem('user', result.user);
-            history.push('/boards');
-        }).catch(error => {
-            console.log(error);
-        });
+        clearState();
+        if (!regexp.test(email)) {
+            setLoginEmailErrorHelperText('Email must be properly formatted');
+            setLoginEmailError(true);
+        } else if (password.length < 6) {
+            setLoginPasswordErrorHelperText('Password must be greater than 6 characters long');
+            setLoginPasswordError(true);
+        } else {
+            auth.signInWithEmailAndPassword(email, password).then(result => {
+                localStorage.setItem('user', result.user);
+                history.push('/boards');
+            }).catch(error => {
+                console.log(error);
+                setloginErrorSnackbar(true);
+            });
+        }
     }
 
     const loginWithGoogle = () => {
@@ -32,6 +55,22 @@ function Login(props) {
         }).catch(error => {
             console.log(error);
         });
+    };
+
+    const clearState = () => {
+        setLoginEmailError(false);
+        setLoginEmailErrorHelperText('');
+        setLoginPasswordError(false);
+        setLoginPasswordErrorHelperText('');
+        setloginErrorSnackbar(false);
+    }
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setloginErrorSnackbar(false);
     };
 
     return (
@@ -49,10 +88,10 @@ function Login(props) {
                             <h2>Log in</h2>
                             <Grid container spacing={3}>
                                 <Grid item xs={12}>
-                                    <TextField id='email' label='Email' type='email' variant='outlined' className={classes.loginTextField} />
+                                    <TextField id='email' error={loginEmailError} helperText={loginEmailErrorHelperText} label='Email' type='email' variant='outlined' className={classes.loginTextField} />
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <TextField id='password' label='Password' variant='outlined' type='password' className={classes.loginTextField} />
+                                    <TextField id='password' error={loginPasswordError} helperText={loginPasswordErrorHelperText} label='Password' variant='outlined' type='password' className={classes.loginTextField} />
                                 </Grid>
                                 <Grid item xs={12}>
                                     <Button variant='contained' color='primary' className={classes.loginButton} onClick={() => signInWithEmailAndPassword(document.getElementById("email").value, document.getElementById("password").value)}>Log in</Button>
@@ -69,6 +108,11 @@ function Login(props) {
                     </Grid>
                 </Grid>
             </Grid>
+            <Snackbar open={loginErrorSnackbar} onClose={handleClose}>
+                <Alert onClose={handleClose} autoHideDuration={6000} severity='error'>
+                    The email and/or password is incorrect. Please try again
+                </Alert>
+            </Snackbar>
         </div>
     );
 }
