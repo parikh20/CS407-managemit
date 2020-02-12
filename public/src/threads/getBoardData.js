678f144772816305bc41f41239c99e620f3cd11f
Adam Chesak <achesak@yahoo.com>
2020-02-12 17:45:59 -0500

    Remove files that shouldn't have existed

--- public/src/threads/getBoardData.js
+++
@@ -1,3 +0,0 @@
-import { expose } from 'threads/workers';
-
-import firebase from '../Firebase';
--- public/src/threads/getColumnGroup.js
+++
@@ -1,7 +0,0 @@
-import { expose } from 'threads/workers';
-
-import firebase from '../Firebase';
-
-expose(async function getColumnGroup(columnGroupId) {
-    
-});
