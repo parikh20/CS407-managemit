04d711579a66b99bef02bd68271c0c09b90a366b
Adam Chesak <achesak@yahoo.com>
2020-02-14 11:56:48 -0500

    Only allow owner to view board settings

--- public/src/pages/BoardSettings.js
+++ public/src/pages/BoardSettings.js
@@ -5,21 +5,17 @@ import { useHistory } from 'react-router-dom';
 import NavBar from './component/NavBar';
 import BoardSettings from './pageBody/BoardSettings';
 
-import firebase from '../Firebase';
+import { db } from '../Firebase';
 
 export default (props) => {
     const viewableHistory = createBrowserHistory();
     const history = useHistory();
     const user = JSON.parse(localStorage.getItem('user'));
 
-    const db = firebase.firestore();
-    db.collection('boards').where('userRefs', 'array-contains', user.email).get().then(snapshot => {
-        for (const doc of snapshot.docs) {
-            if (doc.id === props.match.params.boardId) {
-                return;
-            }
+    db.collection('boards').doc(props.match.params.boardId).get().then(boardRef => {
+        if (!boardRef.exists || boardRef.data().owner !== user.email) {
+            history.push('/boards');
         }
-        history.push('/boards');
     }).catch(err => {
         history.push('/boards');
     });
