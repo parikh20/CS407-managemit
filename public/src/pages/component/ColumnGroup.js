4b944596cef8fa5fa06d6ee1b10c09ff854654ec
Adam Chesak <achesak@yahoo.com>
2020-02-13 19:42:23 -0500

    I'm dumb

--- public/src/pages/component/ColumnGroup.js
+++ public/src/pages/component/ColumnGroup.js
@@ -6,7 +6,7 @@ import Column from './Column.js';
 
 function ColumnGroup(props) {
     return (
-        <div style={{height: 100 + '%', padding: '10px 10px 0px 10px', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden', float: 'left'}}>
+        <div style={{maxWidth: 100 + '%', height: 100 + '%', padding: '10px 10px 0px 10px', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden', float: 'left'}}>
             <GridList style={{flexWrap: 'nowrap'}}>
                 {props.columns.map(column => (
                     <Column key={column.id} board={props.board} column={column} columnName={column.label} />
