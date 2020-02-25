6f2661ccb242f1fcd4d4cb796a10559109fd6af6
Jaineek Parikh <jaineekparikh@yahoo.com>
2020-02-25 18:55:51 -0500

    cloud functions recursiveDelete

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,37 @@
-// const config = require("./util/config");
-// const firebase = require("firebase");
-// firebase.initializeApp(config);
-
 const functions = require("firebase-functions");
-const app = require("express")(); // routing for backend functions
-const cors = require("cors"); // middleware for express
-app.use(cors());
+const firebase_tools = require('firebase-tools');
+// const app = require("express")(); // routing for backend functions
+// const cors = require("cors"); // middleware for express
+// app.use(cors());
 
+exports.recursiveDelete = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: '2GB'
+  })
+  .https.onCall((data, context) => {
 
+    const path = data.path;
+    console.log(
+      `User ${context.auth.uid} has requested to delete path ${path}`
+    );
 
-// index of functions
+    return firebase_tools.firestore
+      .delete(path, {
+        project: process.env.GCLOUD_PROJECT,
+        recursive: true,
+        yes: true,
+      })
+      .then(() => {
+        return {
+          path: path 
+        };
+      });
+  });
 
-// from user.js
-
-// from columns.js
-const {getColumnGroups, createColumnGroups, getColumn, createColumn} = require("./handler/columns");
-app.get("/getColumnGroups", getColumnGroups);
-app.post("/createColumnGroups", createColumnGroups);
-app.get("/getColumn", getColumn);
-app.post("/createColumn", createColumn);
-exports.api = functions.https.onRequest(app);
+// const {getColumnGroups, createColumnGroups, getColumn, createColumn} = require("./handler/columns");
+// app.get("/getColumnGroups", getColumnGroups);
+// app.post("/createColumnGroups", createColumnGroups);
+// app.get("/getColumn", getColumn);
+// app.post("/createColumn", createColumn);
+// exports.api = functions.https.onRequest(app);
