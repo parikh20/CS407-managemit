5878a32ff7f857cba51cbc324c3411fdcd86e4f9
Jaineek Parikh <jaineekparikh@yahoo.com>
2020-03-09 22:56:00 -0400

    Added display and notification settings

--- public/src/pages/component/UserSettings.js
+++ public/src/pages/component/UserSettings.js
@@ -8,8 +8,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import MuiAlert from '@material-ui/lab/Alert';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
-import {Typography } from '@material-ui/core';
-import ArrowIcon from '@material-ui/icons/ArrowForwardIos';
+import {Typography, Button } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
 
 import firebase from '../../Firebase';
 import { db, auth } from '../../Firebase';
@@ -30,6 +30,7 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         textAlign: 'left',
+        marginTop: 20
     },
     settingsCard: {
         cursor: 'pointer',
@@ -48,6 +49,24 @@ function UserSettings(props) {
 
     const user = JSON.parse(localStorage.getItem('user'));
 
+    const [state, setState] = React.useState({
+        darkMode: false,
+        emailNotifications: false,
+        inAppNotifications: false
+      });
+
+    const handleChange = name => event => {
+        setState({ ...state, [name]: event.target.checked });
+        db.collection('users').doc(user.uid).set(
+            {[name]: event.target.checked},
+            {merge: true}
+        ).then(res => {
+
+        }).catch(err => {
+
+        })
+      };
+
     return (
         <div className={classes.userSettingsBody}>
             <Paper className={classes.paper} >
@@ -60,6 +79,58 @@ function UserSettings(props) {
                             <EditPasswordDialog />
                         </div>
                     }
+            </Paper>
+
+            <Paper className={classes.paper} >
+                <Typography variant='h5' className={classes.header}>Display settings</Typography>
+                <Divider />
+                <Grid container spacing={0} className={classes.settingsCard} >
+                    <Grid item xs={12} sm container>
+                        <Grid item container direction="column" spacing={2} >
+                            <Typography variant='subtitle1'>
+                                Dark mode
+                            </Typography>
+                            <Typography variant="body2">
+                                Toggle dark mode
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Switch checked={state.darkModeChecked} onChange={handleChange('darkMode')} value="darkMode" color="primary" />
+                </Grid>
+            <Divider />
+            </Paper>
+
+            <Paper className={classes.paper} >
+                <Typography variant='h5' className={classes.header}>Notification settings</Typography>
+                <Divider />
+                <Grid container spacing={0} className={classes.settingsCard} >
+                    <Grid item xs={12} sm container>
+                        <Grid item container direction="column" spacing={2} >
+                            <Typography variant='subtitle1'>
+                                Email notifications
+                            </Typography>
+                            <Typography variant="body2">
+                                Toggle email notifications
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Switch checked={state.emailNotifications} onChange={handleChange('emailNotifications')} value="emailNotifications" color="primary" />   
+                </Grid>
+
+                <Grid container spacing={0} className={classes.settingsCard} >
+                    <Grid item xs={12} sm container>
+                        <Grid item container direction="column" spacing={2} >
+                            <Typography variant='subtitle1'>
+                                In app notifications
+                            </Typography>
+                            <Typography variant="body2">
+                                Toggle in app notifications
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Switch checked={state.inAppNotifications} onChange={handleChange('inAppNotifications')} value="inAppNotifications" color="primary" />   
+                </Grid>
+            <Divider />
             </Paper>
         </div>
     );
