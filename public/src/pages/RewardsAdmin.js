4eb9a231d7ddb9d39702010d96a03158d4c62e4d
Leon's BigMac <liang268@purdue.edu>
2020-04-30 00:50:59 -0400

    rewards setting page dark style

--- public/src/pages/RewardsAdmin.js
+++ public/src/pages/RewardsAdmin.js
@@ -7,6 +7,13 @@ import BoardSubpageBreadcrumbs from './component/BoardSubpageBreadcrumbs';
 import { cache } from '../Firebase';
 import SetRewards from './pageBody/SetRewards';
 import ViewRedeemedRewards from './pageBody/ViewRedeemedRewards';
+import { makeStyles } from '@material-ui/core/styles';
+
+const primaryDark = "#222831"
+const secondaryDark = "#30476E"
+const darkTextColor = "#c1a57b"
+const black = "#000"
+const white = "#fff"
 
 class RewardsAdmin extends React.Component {
     
@@ -48,7 +55,10 @@ class RewardsAdmin extends React.Component {
     render() {
 
         let shownComponent = this.state.setRewardsPanelShown ? <SetRewards boardRef={this.state.boardRef}></SetRewards> : <ViewRedeemedRewards></ViewRedeemedRewards>;
-
+        const primaryDark = "#222831"
+        const white = "#fff"
+        const mode = localStorage.darkMode
+        mode === 'dark' ? document.body.style.backgroundColor = primaryDark : document.body.style.backgroundColor = white;
         return (
             <div>
                 <NavBar location={this.viewableHistory.location.pathname}  />
@@ -56,8 +66,8 @@ class RewardsAdmin extends React.Component {
                     <Grid item xs={8}>
                         <BoardSubpageBreadcrumbs currentPageName='Rewards Admin' board={this.state.boardRef ? this.state.board : {}} />
                     </Grid>
-                    <Grid item xs={4}>
-                        <Grid container justify="flex-end">
+                    <Grid item xs={4} >
+                        <Grid container justify="flex-end" >
                             <ButtonGroup style={{padding: "10px"}} size='medium'>
                                 <Button onClick={this.setRewardsMode.bind(this)} style={this.state.setRewardsPanelShown ? this.activeButtonStyle : {}}>Set Rewards</Button>
                                 <Button onClick={this.viewRedeemedMode.bind(this)} style={!this.state.setRewardsPanelShown ? this.activeButtonStyle : {}}>Redeemed Rewards</Button>
@@ -67,7 +77,7 @@ class RewardsAdmin extends React.Component {
                 </Grid>
                 <Grid container justify="center">
                     <Grid item xs={10}>
-                        <Paper elevation={3} style={{"min-height": "80vh"}}>
+                        <Paper elevation={3} style={{"min-height": "80vh", backgroundColor:localStorage.darkMode==='dark'?secondaryDark:white, color: localStorage.darkMode==='dark'?darkTextColor:black}}>
                             {this.state.boardRef && shownComponent}
                         </Paper>
                     </Grid>
